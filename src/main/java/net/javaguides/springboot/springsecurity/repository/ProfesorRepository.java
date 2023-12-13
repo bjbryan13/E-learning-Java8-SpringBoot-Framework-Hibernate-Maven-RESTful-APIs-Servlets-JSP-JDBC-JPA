0d7751bf52073664d72e9715ee3ea1e7b012345f
bryaningam4g <brayan@solucionesm4g.com>
2023-12-13 12:10:53 -0500

    Revert "init"
    This reverts commit 8fd5078ceb97eac9ab4517861b75b66718822835.

--- src/main/java/net/javaguides/springboot/springsecurity/repository/ProfesorRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springboot.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.springboot.springsecurity.model.Profesor;
-
-public interface ProfesorRepository  extends JpaRepository<Profesor, Long> {
-
-}
